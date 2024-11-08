37f35a013f406ea255cc75137ee3233024460af4
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-11-08 16:59:42 +1100

    Uncomment the tic code

--- d_main.c
+++ d_main.c
@@ -362,7 +362,7 @@ void D_Display (void)
 bool D_DoomEmscriptenTic (double time, void* userData)
 {
 	// frame syncronous IO operations
-	/*I_StartFrame ();                
+	I_StartFrame ();                
 	
 	// process one or more tics
 	if (singletics)
@@ -395,7 +395,7 @@ bool D_DoomEmscriptenTic (double time, void* userData)
 #ifndef SNDINTR
 	// Update sound output.
 	I_SubmitSound();
-#endif*/
+#endif
 	return true;
 }
 
