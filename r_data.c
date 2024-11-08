42c5ab934a7a56aa376ad29c5a2be223ff1bc4d4
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-11-08 20:09:41 +1100

    DRY violation

--- r_data.c
+++ r_data.c
@@ -680,7 +680,7 @@ int R_FlatNumForName (char* name)
     {
 	namet[8] = 0;
 	memcpy (namet, name,8);
-	I_Error ("R_FlatNumForName: %s not found\nW_CheckNumForName: %s : %d", namet, namet, i);
+	I_Error ("R_FlatNumForName: %s not found\nW_CheckNumForName: %s%s%s%s%s%s%s%s : %d", namet, name[0],name[1],name[2],name[3],name[4],name[5],name[6],name[7],name[8], i);
     }
     return i - firstflat;
 }
